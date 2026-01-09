d62232d2ad0f8488fa9df27b1d55d6a4b0d3caab
Aditi Jha <aditijha@Aditis-Laptop.local>
2026-01-09 14:08:08 +0530

    fix: add inProgress prop to show icon or loader

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1667,6 +1667,15 @@ class ExampleHomePage extends StatelessWidget {
             type: NasikoToastType.info,
           ),
         ),
+        PrimaryButton(
+          label: 'Show In progress Info Toast',
+          onPressed: () => NasikoToastService.show(
+            context,
+            message: 'The new feature roadmap is now available for review.',
+            type: NasikoToastType.info,
+            inProgress: true,
+          ),
+        ),
       ],
     );
   }
